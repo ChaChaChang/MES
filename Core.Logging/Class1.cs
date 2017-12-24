9a6beea6fd2961d482c03f22affd4bbbfbd17a20
Cha Cha Chang <john816930@hotmail.com>
2017-12-24 15:50:40 +0800

    Add core.logging

---
+++ Core.Logging/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Logging
+{
+    public enum Level
+    {
+        Error,
+        Warning,
+        Information,
+        SuccessAudit,
+        FailureAudit
+    }
+
+    public interface ILogger
+    {
+        void Log(Level level,string msg);
+        void Log(Level level, string msg, System.Exception exception);
+
+    }
+
+    public abstract class LogManager : IDisposable
+    {
+        #region Singleton Property
+        // Singleton
+        private static LogManager _current;
+
+        public static LogManager Current
+        {
+            get
+            {
+                // Require
+                if (_current == null) throw new InvalidOperationException();
+
+                // Return
+                return _current;
+            }
+            set
+            {
+                // Require
+                if (_current != null) throw new InvalidOperationException();
+
+                // Return
+                _current = value;
+            }
+        }
+        #endregion
+
+        #region Methods
+        // Methods
+        public abstract ILogger CreateLogger(string name);
+
+        public abstract void Dispose();
+        #endregion
+    }
+
+}
